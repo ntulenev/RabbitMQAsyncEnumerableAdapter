8d44734d1344cf66a0ba5594d91dcc0e8df56133
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-07 21:58:15 +0300

    Add constructor tests

--- src/RabbitMQAsyncEnumerableAdapter/RabbitMQAsyncEnumerableAdapter.Tests/RabbitAdapter.Tests.cs
+++ src/RabbitMQAsyncEnumerableAdapter/RabbitMQAsyncEnumerableAdapter.Tests/RabbitAdapter.Tests.cs
@@ -9,7 +9,7 @@ namespace RabbitMQAsyncEnumerableAdapter.Tests
 {
     public class RabbitAdapterTests
     {
-        [Fact(DisplayName = "The RabbitAdapter can be constructed with buffer size.")]
+        [Fact(DisplayName = "The RabbitAdapter can be constructed with correct buffer size.")]
         [Trait("Category", "Unit")]
         public void CanBeConstructedWithBuffer()
         {
@@ -21,7 +21,7 @@ namespace RabbitMQAsyncEnumerableAdapter.Tests
             exception.Should().BeNull();
         }
 
-        [Fact(DisplayName = "The RabbitAdapter can be constructed with zero buffer size.")]
+        [Fact(DisplayName = "The RabbitAdapter can not be constructed with zero buffer size.")]
         [Trait("Category", "Unit")]
         public void CanNotBeConstructedWithZeroBuffer()
         {
@@ -33,16 +33,64 @@ namespace RabbitMQAsyncEnumerableAdapter.Tests
             exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
         }
 
-        [Fact(DisplayName = "The RabbitAdapter can be constructed with negative buffer size.")]
+        [Fact(DisplayName = "The RabbitAdapter can not be constructed with negative buffer size.")]
         [Trait("Category", "Unit")]
         public void CanNotBeConstructedWithNegativeBuffer()
         {
-            var buffer = 0;
+            var buffer = -1;
 
             var exception = Record.Exception(
                 () => new RabbitAdapter(buffer));
 
             exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+        [Fact(DisplayName = "The RabbitAdapter can be constructed with correct buffer size and ack action.")]
+        [Trait("Category", "Unit")]
+        public void CanBeConstructedWithBufferAndAck()
+        {
+            var buffer = 1;
+
+            var exception = Record.Exception(
+                () => new RabbitAdapter(buffer, _ => { }));
+
+            exception.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "The RabbitAdapter can not be constructed with zero buffer size and ack action.")]
+        [Trait("Category", "Unit")]
+        public void CanNotBeConstructedWithZeroBufferAndAck()
+        {
+            var buffer = 0;
+
+            var exception = Record.Exception(
+                () => new RabbitAdapter(buffer, _ => { }));
+
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+        [Fact(DisplayName = "The RabbitAdapter can not be constructed with negative buffer size and ack action.")]
+        [Trait("Category", "Unit")]
+        public void CanNotBeConstructedWithNegativeBufferAndAck()
+        {
+            var buffer = -1;
+
+            var exception = Record.Exception(
+                () => new RabbitAdapter(buffer, _ => { }));
+
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+        [Fact(DisplayName = "The RabbitAdapter can not be constructed with correct buffer size but null ack action.")]
+        [Trait("Category", "Unit")]
+        public void CanNotBeConstructedWithNullAckAction()
+        {
+            var buffer = 1;
+
+            var exception = Record.Exception(
+                () => new RabbitAdapter(buffer, null!));
+
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
         }
 
     }
