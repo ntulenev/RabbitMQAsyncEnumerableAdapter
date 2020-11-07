b68355da965f3c807887528acdf194eac69e8d50
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-07 21:32:50 +0300

    Add xml comments

--- src/RabbitMQAsyncEnumerableAdapter/RabbitMQAsyncEnumerableAdapter/RabbitAdapter.cs
+++ src/RabbitMQAsyncEnumerableAdapter/RabbitMQAsyncEnumerableAdapter/RabbitAdapter.cs
@@ -8,9 +8,28 @@ using RabbitMQ.Client.Events;
 namespace RabbitMQAsyncEnumerableAdapter
 {
 
+    /// <summary>
+    /// This class provides <see cref="IAsyncEnumerable"/>  behaviour for EAP RabbitMQ logic.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// var adapterBuffer = 100;
+    /// RabbitAdapter ra = new RabbitAdapter(adapterBuffer, messageId => channel.BasicAck(messageId, false));
+    /// consumer.Received += ra.ConsumeData;
+    /// await foreach (var data in ra.WithCancellation(CancellationToken.None))
+    /// {
+    ///     Console.WriteLine(data.Body);
+    /// }   
+    /// </code>
+    /// </example>
     public class RabbitAdapter : IAsyncEnumerable<BasicDeliverEventArgs>
     {
 
+        /// <summary>
+        /// Creates <see cref="RabbitAdapter"/> with internal buffer of messages with size - <paramref name="bufferSize"/>. 
+        /// </summary>
+        /// <param name="bufferSize">Size of immemory messages buffer.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="bufferSize"/> is less then one</exception>
         public RabbitAdapter(int bufferSize)
         {
             if (bufferSize <= 0)
@@ -18,6 +37,13 @@ namespace RabbitMQAsyncEnumerableAdapter
             _channel = Channel.CreateBounded<BasicDeliverEventArgs>(bufferSize);
         }
 
+        /// <summary>
+        /// Creates <see cref="RabbitAdapter"/> with internal buffer of messages with size - <paramref name="bufferSize"/> and acknowledge action.
+        /// </summary>
+        /// <param name="bufferSize">Size of immemory messages buffer.</param>
+        /// <param name="acknowledge">Acknowledge logic.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="acknowledge"/> is null</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="bufferSize"/> is less then one</exception>
         public RabbitAdapter(int bufferSize, Action<ulong> acknowledge)
             :
             this(bufferSize)
@@ -25,10 +51,18 @@ namespace RabbitMQAsyncEnumerableAdapter
             _acknowledge = acknowledge ?? throw new ArgumentNullException(nameof(acknowledge));
         }
 
+        /// <summary>
+        /// Method that should be called from RabbitMQ Received event handler.
+        /// </summary>
+        /// <param name="_">Empty parameter to maintain the contract.</param>
+        /// <param name="e">Message from RabbitMQ.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="e"/> is null</exception>
         public void ConsumeData(object? _, BasicDeliverEventArgs e)
         {
             if (e is null)
+            {
                 throw new ArgumentNullException(nameof(e));
+            }
 
             var spinWait = new SpinWait();
 
@@ -38,14 +72,25 @@ namespace RabbitMQAsyncEnumerableAdapter
             }
 
             if (_acknowledge is not null)
+            {
                 _acknowledge(e.DeliveryTag);
+            }
         }
 
+        /// <summary>
+        /// Creates asynci terator for queue's messages.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Messages async iterator.</returns>
         public async IAsyncEnumerator<BasicDeliverEventArgs> GetAsyncEnumerator(CancellationToken cancellationToken = default)
         {
             while (await _channel.Reader.WaitToReadAsync(cancellationToken).ConfigureAwait(false))
+            {
                 while (_channel.Reader.TryRead(out BasicDeliverEventArgs item))
+                {
                     yield return item;
+                }
+            }
         }
 
         private readonly Action<ulong>? _acknowledge;
