6ce3d2877ccbc98d440cc621c2cab37eb885fdc6
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-07 21:53:34 +0300

    Add Initial test project

---
+++ src/RabbitMQAsyncEnumerableAdapter/RabbitMQAsyncEnumerableAdapter.Tests/RabbitAdapter.Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Xunit;
+
+using FluentAssertions;
+
+
+namespace RabbitMQAsyncEnumerableAdapter.Tests
+{
+    public class RabbitAdapterTests
+    {
+        [Fact(DisplayName = "The RabbitAdapter can be constructed with buffer size.")]
+        [Trait("Category", "Unit")]
+        public void CanBeConstructedWithBuffer()
+        {
+            var buffer = 1;
+
+            var exception = Record.Exception(
+                () => new RabbitAdapter(buffer));
+
+            exception.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "The RabbitAdapter can be constructed with zero buffer size.")]
+        [Trait("Category", "Unit")]
+        public void CanNotBeConstructedWithZeroBuffer()
+        {
+            var buffer = 0;
+
+            var exception = Record.Exception(
+                () => new RabbitAdapter(buffer));
+
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+        [Fact(DisplayName = "The RabbitAdapter can be constructed with negative buffer size.")]
+        [Trait("Category", "Unit")]
+        public void CanNotBeConstructedWithNegativeBuffer()
+        {
+            var buffer = 0;
+
+            var exception = Record.Exception(
+                () => new RabbitAdapter(buffer));
+
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+    }
+}
